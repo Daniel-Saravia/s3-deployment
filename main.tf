6fd1153307cea8b6b67ed5703ffb0c94b792460d
Daniel Saravia <saraviad4444@gmail.com>
2024-12-12 14:54:09 -0700

    enabled static website hosting

--- main.tf
+++ main.tf
@@ -47,6 +47,21 @@ resource "aws_s3_bucket_policy" "allow_public_read" {
   })
 }
 
+#################################
+# Enable Static Website Hosting #
+#################################
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.hellofromterraform.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
 #############################
 # Upload React Build Files  #
 #############################
